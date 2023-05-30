9c310d8c41bc85f9e97d39e22e7667898245a19b
Pelle van der Heide <pelle.vanderheide@eneco.com>
2023-05-30 21:20:44 +0200

    merge sensor and platform
    Create sensors in 'async_setup_entry'

--- goodweproxy/goodweplatform.py
+++ goodweproxy/goodweplatform.py
@@ -1,22 +1,37 @@
 """Goodwe Inverter Logging Proxy component"""
+from __future__ import annotations
 import logging
-
-from .logging_proxy import LoggingProxy
-from .goodwedecoder import decode
-
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from aiohttp import web
 from datetime import datetime, timedelta
 
+from .logging_proxy import LoggingProxy
+from .goodwedecoder import decode
 from .const import *
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.core import callback
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, CoordinatorEntity
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import (
+    ELECTRIC_CURRENT_AMPERE,
+    ELECTRIC_POTENTIAL_VOLT,
+    ENERGY_KILO_WATT_HOUR,
+    FREQUENCY_HERTZ,
+    POWER_WATT,
+    TIME_HOURS,
+    TEMP_CELSIUS,
+)
 
 _LOGGER = logging.getLogger(DOMAIN)
 
-class GoodweProxyCoordinator(DataUpdateCoordinator):
+class GoodWeProxyCoordinator(DataUpdateCoordinator):
     def __init__(self,  *args, serial_number, **kwargs,):
         super().__init__(*args, **kwargs)
         self.serial_number = serial_number
@@ -46,14 +61,78 @@ class GoodweProxyCoordinator(DataUpdateCoordinator):
             _LOGGER.info(f"Skipped message: ({event})")
 
 
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+sensors = {
+    'voltage_pv1': {
+        "uom": ELECTRIC_POTENTIAL_VOLT,
+        "device_class": SensorDeviceClass.VOLTAGE,
+        "state_class": SensorStateClass.MEASUREMENT
+    },
+    'voltage_pv2': {
+        "uom": ELECTRIC_POTENTIAL_VOLT,
+        "device_class": SensorDeviceClass.VOLTAGE,
+        "state_class": SensorStateClass.MEASUREMENT
+    },
+    'current_pv1': {
+        "uom": ELECTRIC_CURRENT_AMPERE,
+        "device_class": SensorDeviceClass.CURRENT,
+        "state_class": SensorStateClass.MEASUREMENT
+    },
+    'current_pv2': {
+        "uom": ELECTRIC_CURRENT_AMPERE,
+        "device_class": SensorDeviceClass.CURRENT,
+        "state_class": SensorStateClass.MEASUREMENT
+    },
+    'voltage_ac': {
+        "uom": ELECTRIC_POTENTIAL_VOLT,
+        "device_class": SensorDeviceClass.VOLTAGE,
+        "state_class": SensorStateClass.MEASUREMENT
+    },
+    'current_ac': {
+        "uom": ELECTRIC_CURRENT_AMPERE,
+        "device_class": SensorDeviceClass.CURRENT,
+        "state_class": SensorStateClass.MEASUREMENT
+    },
+    'freq_ac': {
+        "uom": FREQUENCY_HERTZ,
+        "device_class": SensorDeviceClass.FREQUENCY,
+        "state_class": SensorStateClass.MEASUREMENT
+    },
+    'power_ac': {
+        "uom": POWER_WATT,
+        "device_class": SensorDeviceClass.POWER,
+        "state_class": SensorStateClass.MEASUREMENT
+    },
+    'temperature': {
+        "uom": TEMP_CELSIUS,
+        "device_class": SensorDeviceClass.TEMPERATURE,
+        "state_class": SensorStateClass.MEASUREMENT
+    },
+    'kwh_total': {
+        "uom": ENERGY_KILO_WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "state_class": SensorStateClass.TOTAL_INCREASING
+    },
+    'hours_total': {
+        "uom": TIME_HOURS,
+        "device_class": "on_hours",
+        "state_class": SensorStateClass.TOTAL_INCREASING
+    },
+    'kwh_daily': {
+        "uom": ENERGY_KILO_WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "state_class": SensorStateClass.TOTAL_INCREASING
+    },
+}
+
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities) -> bool:
     """Set up the Goodwe components from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     name = config_entry.title
     serial = config_entry.data[CONF_SERIAL]
     model = f"GW{serial[1:5]}-{serial[5:7]}"
     # Create update coordinator
-    coordinator = GoodweProxyCoordinator(
+    coordinator = GoodWeProxyCoordinator(
         hass,
         _LOGGER,
         name=name,
@@ -77,7 +156,15 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
         KEY_COORDINATOR: coordinator,
         KEY_DEVICE_INFO: device_info,
     }
-    return True
+
+    async_add_entities(
+        GoodWeSensor(
+            coordinator,
+            tag=tag,
+            **sensor,
+            serial_number=serial,
+            device_info=device_info,
+        ) for tag, sensor in sensors.items())
 
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
@@ -96,3 +183,31 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> 
 async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
     """Handle options update."""
     await hass.config_entries.async_reload(config_entry.entry_id)
+
+
+class GoodWeSensor(CoordinatorEntity, SensorEntity):
+    """Representation of a Sensor."""
+
+    @property
+    def available(self) -> bool:
+        return self.coordinator.has_recent_data
+
+    def __init__(self, coordinator, tag, uom, device_class, state_class, serial_number, device_info):
+        super().__init__(coordinator=coordinator)
+        self._attr_name = tag
+        self._attr_native_unit_of_measurement = uom
+        self._attr_device_class = device_class
+        self._attr_state_class = state_class
+        self._attr_unique_id = f"{DOMAIN}-{tag}-{serial_number}"
+        self._attr_device_info = device_info
+
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self._attr_native_value = self.coordinator.data[self._attr_name]
+        self.async_write_ha_state()
+
+
+
+
