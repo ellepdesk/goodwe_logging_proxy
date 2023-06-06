ff413b6d2c8a3bf9d475251d208781bd571024ef
Pelle van der Heide <pelle.vanderheide@eneco.com>
2023-06-06 12:10:53 +0200

    refactor sensor creation

--- custom_components/goodweproxy/sensor.py
+++ custom_components/goodweproxy/sensor.py
@@ -27,68 +27,80 @@ from homeassistant.helpers.update_coordinator import (
 from .const import DOMAIN, GOODWE_COORDINATOR, GOODWE_DEVICE_INFO
 
 
-sensors = {
-    'voltage_pv1': {
+sensors = [
+     {
+        "tag": 'voltage_pv1',
         "uom": ELECTRIC_POTENTIAL_VOLT,
         "device_class": SensorDeviceClass.VOLTAGE,
         "state_class": SensorStateClass.MEASUREMENT
     },
-    'voltage_pv2': {
+    {
+        "tag": 'voltage_pv2',
         "uom": ELECTRIC_POTENTIAL_VOLT,
         "device_class": SensorDeviceClass.VOLTAGE,
         "state_class": SensorStateClass.MEASUREMENT
     },
-    'current_pv1': {
+    {
+        "tag": 'current_pv1',
         "uom": ELECTRIC_CURRENT_AMPERE,
         "device_class": SensorDeviceClass.CURRENT,
         "state_class": SensorStateClass.MEASUREMENT
     },
-    'current_pv2': {
+    {
+        "tag": 'current_pv2',
         "uom": ELECTRIC_CURRENT_AMPERE,
         "device_class": SensorDeviceClass.CURRENT,
         "state_class": SensorStateClass.MEASUREMENT
     },
-    'voltage_ac': {
+    {
+        "tag": 'voltage_ac',
         "uom": ELECTRIC_POTENTIAL_VOLT,
         "device_class": SensorDeviceClass.VOLTAGE,
         "state_class": SensorStateClass.MEASUREMENT
     },
-    'current_ac': {
+    {
+        "tag": 'current_ac',
         "uom": ELECTRIC_CURRENT_AMPERE,
         "device_class": SensorDeviceClass.CURRENT,
         "state_class": SensorStateClass.MEASUREMENT
     },
-    'freq_ac': {
+    {
+        "tag": 'freq_ac',
         "uom": FREQUENCY_HERTZ,
         "device_class": SensorDeviceClass.FREQUENCY,
         "state_class": SensorStateClass.MEASUREMENT
     },
-    'power_ac': {
+    {
+        "tag": 'power_ac',
         "uom": POWER_WATT,
         "device_class": SensorDeviceClass.POWER,
         "state_class": SensorStateClass.MEASUREMENT
     },
-    'temperature': {
+    {
+        "tag": 'temperature',
         "uom": TEMP_CELSIUS,
         "device_class": SensorDeviceClass.TEMPERATURE,
         "state_class": SensorStateClass.MEASUREMENT
     },
-    'kwh_total': {
+    {
+        "tag": 'kwh_total',
         "uom": ENERGY_KILO_WATT_HOUR,
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING
     },
-    'hours_total': {
+    {
+        "tag": 'hours_total',
         "uom": TIME_HOURS,
         "device_class": "on_hours",
         "state_class": SensorStateClass.TOTAL_INCREASING
     },
-    'kwh_daily': {
+    {
+        "tag": 'kwh_daily',
         "uom": ENERGY_KILO_WATT_HOUR,
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING
     },
-}
+]
 
 
 async def async_setup_entry(
@@ -105,19 +117,13 @@ async def async_setup_entry(
     if TYPE_CHECKING:
         assert config_entry.unique_id
 
-    entities = []
-    for sensor in sensors:
-
-
-        entities.append(
-               GoodWeSensor(
-            coordinator,
-            tag=tag,
-            **sensor,
-            config_entry_unique_id=config_entry.unique_id,
-            device_info=device_info,
-        ) for tag, sensor in sensors.items())
-    )
+    entities = [
+    GoodWeSensor(
+        coordinator,
+        sensor,
+        config_entry_unique_id=config_entry.unique_id,
+        device_info=device_info,
+    ) for sensor in sensors]
 
     async_add_entities(entities)
 
@@ -131,18 +137,17 @@ class GoodWeSensor(CoordinatorEntity, SensorEntity):
     def __init__(
             self,
             coordinator: DataUpdateCoordinator,
-            tag,
-            uom,
-            device_class,
-            state_class,
+            sensor,
             config_entry_unique_id,
             device_info,
             ):
         super().__init__(coordinator=coordinator)
+
+        tag = sensor["tag"]
         self._attr_name = tag
-        self._attr_native_unit_of_measurement = uom
-        self._attr_device_class = device_class
-        self._attr_state_class = state_class
+        self._attr_native_unit_of_measurement = sensor["uom"]
+        self._attr_device_class = sensor["device_class"]
+        self._attr_state_class = sensor["state_class"]
         self._attr_unique_id = f"{DOMAIN}-{config_entry_unique_id}-{tag}"
         self._attr_device_info = device_info
 
