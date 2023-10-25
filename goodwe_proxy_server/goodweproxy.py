4ce799955972c9778338ae6368e31a4c8281cb61
Pelle van der Heide <pelle.vanderheide@eneco.com>
2023-10-25 10:43:24 +0200

    goodweproxy:
    fix bug, self IS an application
    extract path from server_request iso hardcode
    Use walrus operator to read chunks
    add missing 'await' to call to forward_request
    remove duplicate, old implementation of post_datalog
    
    add logging on start
    add main for testing in isolation

--- goodwe_proxy_server/goodweproxy.py
+++ goodwe_proxy_server/goodweproxy.py
@@ -1,4 +1,5 @@
 import logging
+import asyncio
 from aiohttp import web
 from aiohttp import ClientSession
 from datetime import datetime as dt
@@ -10,7 +11,7 @@ logger = logging.getLogger(__name__)
 class GoodWeProxy(web.Application):
     def __init__(self, port, datalog_callback):
         super().__init__()
-        self.runner = web.AppRunner(self.proxy)
+        self.runner = web.AppRunner(self)
         self.port = port
         self.datalog_callback = datalog_callback
         self.add_routes([web.post("/Acceptor/Datalog", self.post_datalog)])
@@ -26,9 +27,9 @@ class GoodWeProxy(web.Application):
                 headers = dict(server_request.headers)
                 orig_host = headers.pop('Host')
                 data = await server_request.content.read()
-                logger.warning(f'GOT: data: {data}')
-
-                url = f"https://{orig_host}/Acceptor/Datalog"
+                path = server_request.path
+                url = f"https://{orig_host}/{path}"
+                logger.debug(f'GOT: request, forwarding to {url}, data: {data}')
                 async with session.request(
                         method,
                         url,
@@ -39,10 +40,7 @@ class GoodWeProxy(web.Application):
                     r_headers = dict(client_request.headers)
 
                     client_data = b""
-                    while True:
-                        chunk = await client_request.content.read()
-                        if not chunk:
-                            break
+                    while (chunk := await client_request.content.read()):
                         client_data += chunk
 
             for h in ("Transfer-Encoding", "Connection"):
@@ -66,80 +64,40 @@ class GoodWeProxy(web.Application):
             return event
 
     async def post_sendinterval(self, request):
-        event = self.forward_request(request)
-        logger.info(f"GetSendInterval: {event}")
+        event = await self.forward_request(request)
+        logger.debug(f"GetSendInterval: {event}")
 
     async def post_datalog(self, request):
-        event = self.forward_request(request)
+        event = await self.forward_request(request)
         try:
             data = decode_1phase(event['data'])
             data['raw'] = event['data'].hex()
             data['response_raw'] = event['response'].hex()
+            await self.datalog_callback(data)
         except ValueError:
-            logger.info(f"Skipped message: ({event})")
-        await self.datalog_callback(data)
-
-    async def post_datalog(self, server_request):
-        if server_request.method == "CONNECT":
-            await server_request.read()
-        else:
-            async with ClientSession() as session:
-                method = server_request.method
-                headers = dict(server_request.headers)
-                orig_host = headers.pop('Host')
-                data = await server_request.content.read()
-                logger.warning(f'GOT: data: {data}')
-
-                url = f"http://{orig_host}/Acceptor/Datalog"
-                async with session.request(
-                        method,
-                        url,
-                        data=data,
-                        headers=headers,
-                ) as client_request:
-                    status = client_request.status
-                    r_headers = dict(client_request.headers)
-
-                    client_data = b""
-                    while True:
-                        chunk = await client_request.content.read()
-                        if not chunk:
-                            break
-                        client_data += chunk
-
-
-                for h in ("Transfer-Encoding", "Connection"):
-                    r_headers.pop(h, None)
-                server_response = web.StreamResponse(
-                    status=status,
-                    headers=r_headers
-                )
-                await server_response.prepare(server_request)
-                await server_response.write(client_data)
-
-                event = {
-                    "url": url,
-                    "data": data,
-                    "headers": headers,
-                    "status_code": status,
-                    "response": client_data,
-                    "response_headers": r_headers,
-                    "timestamp": dt.now(),
-                }
-                try:
-                    data = decode_1phase(event['data'])
-                    data['raw'] = event['data'].hex()
-                    data['response_raw'] = event['response'].hex()
-                except ValueError:
-                    logger.info(f"Skipped message: ({event})")
-
-                await self.datalog_callback(data)
-                return server_response
+            logger.debug(f"Skipped message: ({event})")
 
     async def setup(self):
         await self.runner.setup()
+        logger.info(f"Starting webserver at port: {self.port}")
         site = web.TCPSite(self.runner, port=self.port)
         await site.start()
 
     async def stop(self):
         await self.runner.shutdown()
+
+
+if __name__ == "__main__":
+    async def callback(msg):
+        print(msg)
+
+    async def main():
+        try:
+            proxy = GoodWeProxy(port=8099, datalog_callback=callback)
+            await proxy.setup()
+            while True:
+                await asyncio.sleep(1)
+        except (KeyboardInterrupt, asyncio.CancelledError):
+            pass
+
+    asyncio.run(main())
